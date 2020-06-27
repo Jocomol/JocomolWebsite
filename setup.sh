c7af52fc37cba183107c1f4090875c13b10c4ea2
Jocomol <joelmeier08@gmail.com>
2020-06-27 22:48:54 +0200

    added -r

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ WEBFOLDER=/var/www/html
 GITFOLDER=/home/admin/Git/JocomolWebsite
 
 cp $GITFOLDER/index.html $WEBFOLDER
-cp $GITFOLDER/img $WEBFOLDER
+cp -r $GITFOLDER/img $WEBFOLDER
 cp $GITFOLDER/format.css $WEBFOLDER
 cp $GITFOLDER/favicon.ico $WEBFOLDER
 
