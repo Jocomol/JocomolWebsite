bd72bd8fa92a9687a191d9dc236551455b74420b
Jocomol <joelmeier08@gmail.com>
2020-06-27 22:47:36 +0200

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@ WEBFOLDER=/var/www/html
 GITFOLDER=/home/admin/Git/JocomolWebsite
 
 cp $GITFOLDER/index.html $WEBFOLDER
-cp $GITFOLDER/pictures $WEBFOLDER
+cp $GITFOLDER/img $WEBFOLDER
+cp $GITFOLDER/format.css $WEBFOLDER
+cp $GITFOLDER/favicon.ico $WEBFOLDER
 
 certbot renew
