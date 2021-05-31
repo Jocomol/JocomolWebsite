f734120be63a5947835e658feca6fa49bc074999
Jocomol <joelmeier08@gmail.com>
2021-05-31 10:19:52 +0200

    Uploaded essay

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ GITFOLDER=/home/webadmin/Git/JocomolWebsite
 
 cp $GITFOLDER/index.html $WEBFOLDER
 cp -r $GITFOLDER/img $WEBFOLDER
+cp -r $GITFOLDER/files $WEBFOLDER
 cp $GITFOLDER/format.css $WEBFOLDER
 cp $GITFOLDER/favicon.ico $WEBFOLDER
 
